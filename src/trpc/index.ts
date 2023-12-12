96d8cc9c4923782e3535722ef5bed50e9fff7a4b
moritzWa <wallawitsch@gmail.com>
2023-12-11 21:57:24 -0800

    getUserFiles route

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,8 @@
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { TRPCError } from '@trpc/server'
-import { publicProcedure, router } from './trpc'
+import { z } from 'zod'
+import { privateProcedure, publicProcedure, router } from './trpc'
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -36,6 +37,37 @@ export const appRouter = router({
 
     return { success: true }
   }),
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    })
+  }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      })
+
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      })
+
+      return file
+    }),
 })
 
 export type AppRouter = typeof appRouter
