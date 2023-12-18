47f4a4b80ace9b0e50c87395be1161f7af59cad4
moritzWa <wallawitsch@gmail.com>
2023-12-17 20:04:58 -0800

    continue page view

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,3 +1,5 @@
+import ChatWrapper from '@/components/ChatWrapper'
+import PdfRenderer from '@/components/PdfRenderer'
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { notFound, redirect } from 'next/navigation'
@@ -27,7 +29,28 @@ const Page = async ({ params }: PageProps) => {
 
   //   const plan = await getUserSubscriptionPlan()
 
-  return <>{file.name}</>
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* Left sidebar & main wrapper */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* Main area */}
+            <PdfRenderer
+            // url={file.url}
+            />
+          </div>
+        </div>
+
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper
+          // isSubscribed={plan.isSubscribed}
+          // fileId={file.id}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Page
