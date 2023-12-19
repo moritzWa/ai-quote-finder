db900311c0ca7127c4f429477464e5a4e8e0efa3
moritzWa <wallawitsch@gmail.com>
2023-12-19 13:07:16 -0800

    pdf vector indexing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,8 +1,22 @@
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { createUploadthing, type FileRouter } from 'uploadthing/next'
+import { FileRouter, createUploadthing } from 'uploadthing/next'
+
+// import PDFLoader from langchain
+// import { getPineconeClient } from '@/lib/pinecone'
+import { Pinecone } from '@pinecone-database/pinecone'
+import { UploadStatus } from '@prisma/client'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
 
 const f = createUploadthing()
+
+const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY!,
+  // environment: 'us-east1-gcp',
+  environment: 'gcp-starter',
+})
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -17,7 +31,7 @@ export const ourFileRouter = {
       // If you throw, the user will not be able to upload
       if (!user || !user.id) throw new Error('Unauthorized')
 
-      // Whatever is ret urned here is accessible in onUploadComplete as `metadata`
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id }
     })
     .onUploadComplete(async ({ metadata, file }) => {
@@ -30,6 +44,54 @@ export const ourFileRouter = {
           uploadStatus: 'PROCESSING',
         },
       })
+
+      try {
+        // load pdf into memory
+        const pdfRes = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+        )
+        const blob = await pdfRes.blob()
+        const loader = new PDFLoader(blob)
+
+        // extract text
+        const pageLevelDocs = await loader.load()
+        const amountOfPages = pageLevelDocs.length
+        // TODO: err if too many pages / free plan
+
+        // vectorize text
+        // const pinecone = await getPineconeClient()
+        const pineconeIndex = pinecone.Index('ai-quote-finder')
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        })
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createFile.id,
+        })
+
+        // update file
+
+        await db.file.update({
+          where: {
+            id: createFile.id,
+          },
+          data: {
+            uploadStatus: UploadStatus.SUCCESS,
+          },
+        })
+      } catch (err) {
+        console.error('err in onUploadComplete', err)
+        await db.file.update({
+          where: {
+            id: createFile.id,
+          },
+          data: {
+            uploadStatus: UploadStatus.FAILED,
+          },
+        })
+      }
     }),
 } satisfies FileRouter
 
