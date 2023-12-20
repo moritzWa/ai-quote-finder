a85506e96ed97c424005852036d5f18bcbee1bf2
moritzWa <wallawitsch@gmail.com>
2023-12-19 18:35:55 -0800

    chat onMutate

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,5 +1,7 @@
+import { trpc } from '@/app/_trpc/client'
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
 import { useMutation } from '@tanstack/react-query'
-import { ReactNode, createContext, useState } from 'react'
+import { ReactNode, createContext, useRef, useState } from 'react'
 
 type StreamResponse = {
   addMessage: () => void
@@ -24,6 +26,12 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   const [message, setMessage] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
+  const utils = trpc.useContext()
+
+  // const { toast } = useToast()
+
+  const backupMessage = useRef('')
+
   const { mutate: sendMessage } = useMutation({
     mutationFn: async ({ message }: { message: string }) => {
       const response = await fetch('/api/message', {
@@ -39,6 +47,69 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
       }
 
       return response.body
+    },
+    onMutate: async ({ message }) => {
+      backupMessage.current = message
+      setMessage('')
+
+      // cancel outbound calls to prevent overwriting optimistic update
+      await utils.getFileMessages.cancel()
+
+      // snapshot previous messages
+      const previousMessages = utils.getFileMessages.getInfiniteData()
+
+      // optimistic insert new message
+      utils.getFileMessages.setInfiniteData(
+        { fileId, limit: INFINITE_QUERY_LIMIT },
+        (old) => {
+          if (!old) {
+            return {
+              pages: [],
+              pageParams: [],
+            }
+          }
+
+          let newPages = [...old.pages]
+
+          let latestPage = newPages[0]!
+
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true,
+            },
+            ...latestPage.messages,
+          ]
+
+          newPages[0] = latestPage
+
+          return {
+            ...old,
+            pages: newPages,
+          }
+        },
+      )
+      // trigger AI thinking loading spinner
+      setIsLoading(true)
+
+      return {
+        previousMessages: previousMessages?.pages.flatMap(
+          (page) => page.messages,
+        ),
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current)
+      utils.getFileMessages.setData(
+        { fileId },
+        { messages: context?.previousMessages ?? [] },
+      )
+    },
+    onSettled: async () => {
+      setIsLoading(false)
+      await utils.getFileMessages.invalidate({ fileId })
     },
   })
 
