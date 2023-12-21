a1686aa4e949eac69eeac3b92c0dd246794ab7db
moritzWa <wallawitsch@gmail.com>
2023-12-20 17:27:15 -0800

    pass pricing into chatwrapper

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,6 +1,7 @@
 import PdfRenderer from '@/components/PdfRenderer'
 import ChatWrapper from '@/components/chat/ChatWrapper'
 import { db } from '@/db'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { notFound, redirect } from 'next/navigation'
 
@@ -27,7 +28,7 @@ const Page = async ({ params }: PageProps) => {
 
   if (!file) notFound()
 
-  //   const plan = await getUserSubscriptionPlan()
+  const plan = await getUserSubscriptionPlan()
 
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
@@ -41,10 +42,7 @@ const Page = async ({ params }: PageProps) => {
         </div>
 
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper
-            // isSubscribed={plan.isSubscribed}
-            fileId={file.id}
-          />
+          <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
         </div>
       </div>
     </div>
