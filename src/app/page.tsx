9c523590a4ada555fb28b1864788592466b75142
moritzWa <wallawitsch@gmail.com>
2023-12-22 16:30:33 -0800

    add better landing page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,12 +64,12 @@ export default function Home() {
               <div className="mt-16 flow-root sm:mt-24">
                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <Image
-                    src="/dashboard-preview.jpg" // TODO: replace with actual image
+                    src="/product-preview.png" // TODO: replace with actual image
                     alt="product preview"
                     width={1364}
                     height={866}
                     quality={100}
-                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                    className="rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10"
                   />
                 </div>
               </div>
