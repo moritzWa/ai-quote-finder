1d03e350e65a38b6e54783f54785d7355c6649ce
moritzWa <wallawitsch@gmail.com>
2024-01-31 19:10:48 -0800

    cleanup preview

--- src/components/PdfPreview.tsx
+++ src/components/PdfPreview.tsx
@@ -6,16 +6,18 @@ import { Document, Thumbnail, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
 const PdfPreview = ({ file }: { file: { url: string } }) => {
-  const [numPages, setNumPages] = useState<number>()
-
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages)
-  }
+  const [loading, setLoading] = useState(true)
 
   return (
     <div>
-      <Document file={file.url} onLoadSuccess={onDocumentLoadSuccess}>
-        <Thumbnail pageNumber={1} height={200} width={100} />
+      <Document file={file.url}>
+        <Thumbnail
+          pageNumber={1}
+          height={200}
+          width={100}
+          // figure out why this is just saying loading instead of showing the skeleton
+          // oading={<Skeleton className="h-16" />}
+        />
       </Document>
     </div>
   )
