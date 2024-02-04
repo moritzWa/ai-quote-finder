bf3cdc1878194aef0a3e9a34840daef416d284e3
moritzWa <wallawitsch@gmail.com>
2024-02-03 17:27:42 -0800

    fix link bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,6 @@ export default function Home() {
             className: 'mt-5',
           })}
           href="/dashboard"
-          target="_blank"
         >
           Get started
           <ArrowRight className="ml-2 h-5 w-5" />
