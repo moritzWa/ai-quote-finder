358d595adc9563021849fb2f9da71ca5fd6c2802
moritzWa <wallawitsch@gmail.com>
2024-02-03 18:17:52 -0800

    navbar polish

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-      <MaxWidthWrapper className={user ? 'md:px-10' : ''}>
+      <MaxWidthWrapper className={!!user ? 'md:px-10' : ''}>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
             <SearchIcon className="h-6 w-6 mr-2" />
