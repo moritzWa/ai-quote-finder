b823581e75305f2da3b041a7000351140855d579
moritzWa <wallawitsch@gmail.com>
2024-03-26 17:42:16 -0700

    new hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,91 +2,58 @@ import { buttonVariants } from '@/components/ui/button'
 import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import MaxWidthWrapper from '../components/MaxWidthWrapper'
 
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper className="mb-12 mt-8 sm:mt-20 flex flex-col items-center justify-center text-center">
-        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-          <p className="text-sm font-semibold text-gray-700">
-            Quote Finder is now public!
-          </p>
-        </div>
-        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Find any <span className=" text-purple-600">quote</span> or{' '}
-          <span className=" text-purple-600">book section</span> in seconds.
-        </h1>
-        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Quote Finder allows you to find the best parts of your favorite books.
-          Upload your books or find sections in a pre-selected collection of
-          excellect non-fiction books.
-        </p>
-
-        <Link
-          className={buttonVariants({
-            size: 'lg',
-            className: 'mt-5',
-          })}
-          href="/dashboard"
-        >
-          Get started
-          <ArrowRight className="ml-2 h-5 w-5" />
-        </Link>
-      </MaxWidthWrapper>
-
-      {/* value proposition section */}
-      <div>
-        <div className="relative isolate">
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-bottom-10"
-          >
-            <div
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#6a5fff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            />
-            <div
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-              className="relative left-[calc(30%-30rem)] aspect-[1155/678] w-[26.125rem] -translate-x-5 bg-gradient-to-tr from-[#77b8de] to-[#a25fff] opacity-50 sm:left-[calc(50%-20rem)] sm:w-[72.1875rem]"
-            />
-          </div>
-
-          <div>
-            <div className="mx-auto w-fit px-6 lg:px-8">
-              <div className="mt-16 flow-root sm:mt-24">
-                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                  <Image
-                    src="/product-preview.png"
-                    alt="product preview"
-                    width={864}
-                    height={766}
-                    quality={100}
-                    className="rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10"
-                  />
+      <div className="bg-white">
+        <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20">
+          <div className="mx-auto max-w-7xl pb-24 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32">
+            <div className="px-6 lg:px-0 lg:pt-4">
+              <div className="mx-auto max-w-2xl">
+                <div className="max-w-lg">
+                  <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Find any <span className=" text-purple-600">quote</span> or{' '}
+                    <span className=" text-purple-600">book section</span> in
+                    seconds.
+                  </h1>
+                  <p className="mt-6 text-lg leading-8 text-gray-600">
+                    Quote Finder allows you to find the best parts of your
+                    favorite books. Upload your books or find sections in a
+                    pre-selected collection of excellect non-fiction books.
+                  </p>
+                  <div className="mt-5 flex items-center just gap-x-6">
+                    <Link
+                      className={buttonVariants({
+                        size: 'lg',
+                        className: 'mt-5',
+                      })}
+                      href="/dashboard"
+                    >
+                      Get started
+                      <ArrowRight className="ml-2 h-5 w-5" />
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen">
+              <div
+                className="absolute inset-y-0 right-1/2 -z-10 -mr-10 w-[200%] skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 md:-mr-20 lg:-mr-36"
+                aria-hidden="true"
+              />
+              <div className="shadow-lg md:rounded-3xl inline-block mx-auto p-0">
+                <Image
+                  src="/demo-short.gif"
+                  alt="demo gif"
+                  height={500}
+                  width={643}
+                  className="h-full w-full rounded-lg object-cover md:h-[500px] md:w-[643px] lg:h-[500px] lg:w-[643px]"
+                />
+              </div>
+            </div>
           </div>
-
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr to-[#ff80b5] from-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
-            />
-          </div>
+          <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
         </div>
       </div>
     </>
