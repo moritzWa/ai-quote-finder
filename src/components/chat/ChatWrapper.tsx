0fea7ac8c580886185ca8ed953d8c58d127f2b46
moritzWa <wallawitsch@gmail.com>
2024-03-29 19:05:05 -0700

    fix micro copy to incpr epubs

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -47,7 +47,7 @@ const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
             <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
             <h3 className="font-semibold text-xl">Loading...</h3>
             <p className="text-zinc-500 text-sm">
-              We&apos;re preparing your PDF.
+              We&apos;re preparing your File.
             </p>
           </div>
         </div>
@@ -62,7 +62,7 @@ const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
         <div className="flex-1 flex justify-center items-center flex-col mb-28">
           <div className="flex flex-col items-center gap-2">
             <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
-            <h3 className="font-semibold text-xl">Processing PDF...</h3>
+            <h3 className="font-semibold text-xl">Processing File...</h3>
             <p className="text-zinc-500 text-sm">This won&apos;t take long.</p>
           </div>
         </div>
@@ -77,7 +77,7 @@ const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
         <div className="flex-1 flex justify-center items-center flex-col mb-28">
           <div className="flex flex-col items-center gap-2">
             <XCircle className="h-8 w-8 text-red-500" />
-            <h3 className="font-semibold text-xl">Too many pages in PDF</h3>
+            <h3 className="font-semibold text-xl">Too many pages in File</h3>
             <p className="text-zinc-500 text-sm">
               Your{' '}
               <span className="font-medium">
@@ -85,7 +85,7 @@ const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
               </span>{' '}
               plan supports up to{' '}
               {isSubscribed ? proPlan.pagesPerPdf : freePlan.pagesPerPdf} pages
-              per PDF.
+              per File.
             </p>
             <div className="flex gap-2 mt-4">
               <Link
