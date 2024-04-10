f271507326a0d579f15a63e07baee3c86863b7e6
moritzWa <wallawitsch@gmail.com>
2024-04-10 10:44:15 -0700

    polish blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -75,21 +75,30 @@ const DynamicVideoPlayer = dynamic(() => import('@/components/VideoPlayer'), {
 export const posts: PostType[] = [
   {
     id: 1,
-    title: 'The best apps to chat with your Books',
+    title: 'The 7 Best Apps to Chat With Your Books',
     slug: 'the-best-apps-to-chat-with-your-books',
     description:
-      'Comparing 7 apps to chat with your books and search for content in them semantically.',
+      'Comparing the best apps to chat with your books and search for content in them semantically.',
     date: 'April 4, 2024',
     datetime: '2024-04-08',
     content: (
       <>
         <p>
+          Imagine engaging in a conversation with the author of your favorite
+          non-fiction book, or effortlessly returning to the most intriguing
+          sections without having made any highlights or bookmarks. With the
+          advent of AI, this is not as far-fetched as it sounds.
+        </p>
+        <p>
           There are several apps that use AI to help you interact with your
-          books. Two major use-cases are asking questions to your book to
-          understand its content better and finding quotes or sections within a
-          book. In this blog post we compare 7 of them. Other criteria we will
-          examine are the number of file types the applications supports and its
-          subscription price.
+          books. The primary uses include querying your book for a deeper
+          understanding of its content and effortlessly locating specific quotes
+          or sections.
+        </p>
+        <p>
+          In this blog post we compare 6 of them. We&apos;ll also evaluate them
+          based on the diversity of file types supported and the affordability
+          of their subscription models.
         </p>
 
         <h2>AI Quote Finder</h2>
@@ -99,7 +108,9 @@ export const posts: PostType[] = [
           </a>{' '}
           is a web app that uses AI to help you to find specific parts of your
           favorite books in seconds. You can also use it to understand books
-          with the AI chat feature. It supports EPUB and PDF files.
+          with the AI chat feature. It supports EPUB and PDF files. This
+          flexibility ensures that whether you&apos;re a fan of EPUB or PDF
+          formats, your entire library remains accessible.
         </p>
         <p>
           With a generous free plan and only $10 for unlimited access, it is the
@@ -142,7 +153,7 @@ export const posts: PostType[] = [
             scripts
           </a>{' '}
           to embed books and make them searchable. The problems with this
-          solution are that 1. the file is not stored. After you close the tab
+          solution are that: 1. the file is not stored. After you close the tab
           the file is deleted. 2. Each time you want to run the script you have
           to manually trigger the execution of each sub-process (run each cell
           of the notebook) which takes a lot of time. Besides these points it
@@ -163,7 +174,7 @@ export const posts: PostType[] = [
           month.
         </p>
         <Image src="/lightpdf.png" alt="open ai" width={500} height={1000} />
-        <h2>Colab Notebook</h2>
+        <h2>ChatGPT</h2>
         <p>
           If you pay $20 per month you can get access to ChatGPT&apos;s file
           upload feature. Open AI states that ChatGPT&apos;s supports &quot;All
@@ -173,7 +184,6 @@ export const posts: PostType[] = [
           but fails miserably.
         </p>
         <Image src="/open-ai.png" alt="open ai" width={700} height={1000} />
-        <h2>ChatGPT</h2>
         <p>
           We also tried uploading the PDF of the book &quot;Zero to One&quot; by
           Peter Thiel but ChatGPT failed to find any information about our
@@ -199,6 +209,24 @@ export const posts: PostType[] = [
           width={700}
           height={1000}
         />
+        <h2>Conclusion</h2>
+        <p>
+          In concluding our journey through innovative apps that enhance our
+          interaction with books, it&apos;s evident that AI has transformed how
+          we engage with literature. Among the impressive options, AI Quote
+          Finder shines as the superior choice. Its affordability and broad file
+          format support make it accessible and practical for a wide audience.
+          Notably, its advanced semantic search capability allows users to dive
+          deep into their favorite texts, uncovering insights and finding
+          passages with ease.
+        </p>
+        <p>
+          AI Quote Finder stands out by not only making literature more
+          accessible but also by enriching our reading experience through
+          intelligent, nuanced conversations with our books. In this new era of
+          literary exploration, AI Quote Finder is a beacon for avid readers
+          seeking a deeper connection with their reading material.
+        </p>
         <CTASection className="mt-10" />
       </>
     ),
